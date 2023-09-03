3f94664f6b2ec7ac5779ff1538561f745c1445e6
Vishnu Bhargav Bandaru <vichu259@gmail.com>
2023-09-03 00:59:14 -0700

    first commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,78 @@
-import Image from 'next/image'
+"use client";
+import {Card, Divider, CardHeader, CardBody, CardFooter, Image} from "@nextui-org/react";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+    <main>
+      <HeadBar></HeadBar>
+      <Page1></Page1>
+      <Projects></Projects>
     </main>
   )
+}
+
+function HeadBar() {
+  return(
+    <div className="flex h-16 w-full items-center justify-between sticky top-0 bg-background">
+      <div className="flex cursor-pointer">
+        <img className="h-10 ml-10 mr-5" src="/logo.svg"></img>
+        <LogoText/>
+      </div>
+    </div>
+  );
+}
+
+function LogoText() {
+  const logo = ["D", "O", "C", "K"];
+  return (
+    <p className="text-2xl flex font-semibold">
+      {
+        logo.map(element => {
+          return <span className="p-0.5">{element}</span>
+        })
+      }
+    </p>);
+}
+
+function Page1() {
+  return(<div className="m-8 explore-background h-[70vh]">
+    <div className="w-3/6">
+      <div className="pt-8 pb-8 text-5xl"><span style={{"color":"#586994"}}>LEARN</span> THROUGH PROJECTS</div>
+      <div className="text-xl">Ready to ditch the textbooks and dive into the world of coding with your sleeves rolled up? Welcome to a journey where we learn by doing, one project at a time. Whether you're into software wizardry, full-stack finesse, or AI enchantment, get ready to unleash your inner developer. Let's code, create, and conquer!</div><br/>
+    </div>
+  </div>);
+}
+
+function Projects() {
+  return (<div className="m-8">
+    <div className="text-2xl mb-5 font-semibold" style={{"color":"#586994"}}> FULL STACK </div>
+    <div className="flex gap-4">
+      <Card className="w-[300px] cursor-pointer">
+        <CardHeader>
+          <div className="font-bold flex justify-center w-full">
+            <span>Project 1 : Doggie Facts</span>
+          </div>
+        </CardHeader>
+        <Divider></Divider>
+        <CardBody>
+          <Image alt="Doggie Facts"
+          className="object-cover"
+          src="/doggie-facts.jpeg"></Image>
+        </CardBody>
+      </Card>
+      <Card className="w-[300px] cursor-pointer">
+        <CardHeader>
+          <div className="font-bold flex justify-center w-full">
+            <span>Project 2 : Tic Tac Toe</span>
+          </div>
+        </CardHeader>
+        <Divider></Divider>
+        <CardBody>
+          <Image alt="Tic Tac Toe"
+          className="object-cover"
+          src="/tic-tac-toe.png"></Image>
+        </CardBody>
+      </Card>
+    </div>
+  </div>);
 }
